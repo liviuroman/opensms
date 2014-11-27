7e8798382dc4393af904ce6fd3608721b438face
Marian <memelea@gmail.com>
2014-11-27 23:50:28 +0200

    environment variables demo file

---
+++ env.demo.php
@@ -0,0 +1,51 @@
+<?php
+
+return array(
+
+  /*
+  **  app/config/app.php - line 81
+  */
+  'encription_key' => 'asd',
+
+  /*
+  **  app/config/database.php - from line 55
+  */
+  'mysql_host' => 'localhost',
+  'mysql_database' => 'asd',
+  'mysql_username' => 'asd',
+  'mysql_password' => 'asd',
+
+
+  /*
+  **  app/config/mail.php - from line 83
+  */
+  'mail_username' => 'asd',
+  'mail_password' => 'asd',
+
+
+  /*
+  **  app/config/remote.php - from line 33
+  */
+  'remote_host' => 'asd',
+  'remote_username' => 'asd',
+  'remote_password' => 'asd',
+
+  /*
+  **  app/config/packages/bugsnag/bugsnag-laravel/config.php - line 6
+  */
+  'bugsnag_api' => 'asd',
+
+  /*
+  ** app/config/queue.php - from line 52
+  */
+  'iron_token' => 'asd',
+  'iron_projectid' => 'asd',
+
+  /*
+  ** app/classes/Queue.php (BOXCAR API CODE) - line 9
+  */
+  'boxcar_api' => 'asd',
+
+
+
+);
